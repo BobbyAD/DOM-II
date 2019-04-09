3dea97f5a4cc601c358c1186498f92faca3dfbc7
Robert Driskell <driskell.ba@gmail.com>
2019-04-09 17:39:15 -0500

    Finished MVP and GSAP stretch

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ const nav = document.querySelector('nav');
 const navLink = document.querySelector('.nav-link');
 const logo = document.querySelector('.logo-heading');
 const intro = document.querySelector('.intro');
-
+const html = document.querySelector('html');
 
 //Functions
 function rotationYComplete(event) {
@@ -43,7 +43,7 @@ function percentCheck() {
 
 
 //Listeners
-//Count: 8
+//Count: 11
 funBus.addEventListener('mouseover', event => {
     event.target.style.filter = "invert(100%)";
     TweenMax.to(".intro img", 3, {rotationY:180, onComplete: rotationYComplete, onCompleteParams:[event]});
@@ -84,6 +84,11 @@ boatImg.addEventListener('click', event => {
     event.preventDefault();
 });
 
+boatImg.addEventListener('dragstart', event => {
+    console.log(event);
+    event.target.style.filter = `hue-rotate(${getRandomInt(0,360)}deg)`;
+})
+
 //You can nest these :O
 logo.addEventListener('mouseover', event => {
     var original = event.target.textContent;
@@ -100,6 +105,19 @@ intro.addEventListener('dblclick', event => {
 
 intro.addEventListener('mousedown', event => {
     event.preventDefault();
+})
+
+window.addEventListener('focus', event => {
+    html.style.backgroundColor = 'white';
+    window.addEventListener('blur', event => {
+        html.style.backgroundColor = 'grey';
+    })
+})
+
+window.addEventListener('keydown', event => {
+    if (event.key == 'r') {
+        html.style.backgroundColor = `rgb(${getRandomInt(0,255)},${getRandomInt(0,255)},${getRandomInt(0,255)})`;
+    }
 })
 
 //This is in the assignment, but it seems evil
