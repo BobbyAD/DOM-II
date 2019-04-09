b185fc22b706626571c0ef77e1825b574ea90b38
Robert Driskell <driskell.ba@gmail.com>
2019-04-09 15:33:12 -0500

    First listener added, got GSAP working to make coming up with events easier

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+//Selectors
+const funBus = document.querySelector('.intro img');
+
+//Listeners
+funBus.addEventListener('mouseover', event => {
+    event.target.style.filter = "invert(100%)";
+    TweenMax.to(".intro img", 3, {rotationY:180});
+});
+
+funBus.addEventListener('mouseleave', event => {
+    event.target.style.filter = "invert(0%)";
+    TweenMax.to(".intro img", 3, {rotationY:0});
+});
+
